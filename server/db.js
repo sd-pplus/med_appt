72c97262fd52bb8df456a60146af30d0edb1247e
sd-pplus <sandep.dinesh@protonmail.com>
2026-07-21 11:50:12 -0400

    DB update

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:wEWt52w1zRHzLT4AvYzz342e@172.21.112.13:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
