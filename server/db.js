a51df3d9b364178777f0e598646b19956637d04b
sd-pplus <sandep.dinesh@protonmail.com>
2026-07-24 09:49:14 -0400

    db and config updates

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:3VUBtOs8QcG0jxZmXn0pEAKB@172.21.78.98:27017";
+const mongoURI =  "mongodb://root:1pIiH66ZAvOKBHqdxhnt16Yo@172.21.93.187:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "https://sandeepqaops-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
+export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "https://sandeepqaops-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
 console.log(
     "API_URL :",
     API_URL
